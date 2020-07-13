63bec0ea397363319aa0874cc6c89327b1e03e8d
Javier Concha <jaconcha@gmail.com>
2020-07-13 17:02:50 +0200

    S3A+S3B Coverage for a {str(ncycle)}-day cycle

--- src/sat_coverage.py
+++ src/sat_coverage.py
@@ -19,6 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import os.path
@@ -30,9 +31,6 @@ import shapely.geometry
 import cartopy.io.shapereader as shapereader
 import pandas as pd
 from matplotlib.lines import Line2D
-
-# import cartopy.crs as ccrs
-# from descartes import PolygonPatch
 
 def create_map():
     lat_min = 10
@@ -50,21 +48,22 @@ def create_map():
     m.fillcontinents(color='grey')
     return m
 
-def draw_polygon(lats,lons,m,sensor):
+def draw_polygon(lats,lons,m,sensor,flag_all=False):
     x, y = m( lons, lats )
     xy = [(x[0],y[0]),(x[1],y[1]),(x[2],y[2]),(x[3],y[3])]
     
-    if sensor == 'S3A':
-        fc = 'red'
-    elif sensor == 'S3B':
+    if not flag_all:
+        if sensor == 'S3A':
+            fc = 'red'
+        elif sensor == 'S3B':
+            fc = 'blue'
+    else:
         fc = 'blue'
+        
     poly = matplotlib.patches.Polygon( xy, facecolor=fc, alpha=0.2,closed=True, ec='k', lw=1)
     plt.gca().add_patch(poly)
     poly_all = shapely.geometry.Polygon(xy)
     return m, poly_all
-
-
-
     
 def create_MED_shp(path_to_shp):
     #find the MED polygon. 
@@ -170,11 +169,50 @@ def coverage_calc(date,S3Apoly, S3Bpoly):
     
     plt.gcf()
     plt.title(f'{date}; A$\cup$B={AB_union_perc:.2f}%; A$\cap$B={AB_inter_perc:.2f}%')
+    plt.close()
     
     print(f'Coverage A+B: {AB_union_perc:.2f}%')
     print(f'Coverage A intersection B: {AB_inter_perc:.2f}%')
     
-    return AB_union_perc, AB_inter_perc       
+    return AB_union_perc, AB_inter_perc  
+
+def create_all_polygon(df_poly):
+    plt.figure()
+    m = create_map()
+    polygons = []
+    for row in df_poly.itertuples(index=True, name='Pandas'):
+        # extract data from dataframe
+        UL_lat = row.UL_lat
+        UL_lon = row.UL_lon
+        UR_lat = row.UR_lat
+        UR_lon = row.UR_lon
+        LL_lat = row.LL_lat
+        LL_lon = row.LL_lon
+        LR_lat = row.LR_lat
+        LR_lon = row.LR_lon    
+        sensor = row.sensor
+
+        # draw map
+        lats_poly =[UL_lat,UR_lat,LR_lat,LL_lat]
+        lons_poly =[UL_lon,UR_lon,LR_lon,LL_lon]
+        m, poly = draw_polygon(lats_poly,lons_poly,m,sensor,flag_all=True)
+        polygons.append(poly)
+    return polygons     
+
+def intersection_with_polygon(polygons,positions):
+    count_vec = []
+    count = 0
+    
+    for n in range(positions.shape[1]):
+        point = shapely.geometry.Point(positions[1,n],positions[0,n])
+     
+        for p in polygons:
+            if point.within(p):
+                count += 1
+        count_vec.append(count)
+        count = 0        
+            
+    return count_vec
 #%%
 # def main():
 if sys.platform == 'linux': 
@@ -186,28 +224,76 @@ elif sys.platform == 'darwin':
 else:
     print('Error: host flag is not either mac or vm')   
 
-    # create Dataframe    
+# create Dataframe    
+# per percentage coverage    
 cols = ['date','A union B','A interc B']
 df_coverage = pd.DataFrame(columns = cols) 
+
+# per saving polygons
+cols = ['doy','A','B','A+B','A^B','(A+B)-(A^B)']
+df_possible = pd.DataFrame(columns = cols)
+
+ABpoly = shapely.geometry.Polygon()
   
 csv_filename = os.path.join(path_out,'OLCI_geo_info_ALL.csv')
 df = pd.read_csv(csv_filename)
 
 year_str = '2020'
-sdoy = 148
-edoy = 183
+sdoy = 1
+edoy = 2
 
 for doy in range(sdoy,edoy+1):
-    
-    
     if not df.loc[df['doy'] == doy].empty:
         df_doy = df.loc[df['doy'] == doy]
         print('-----------')
         print(df_doy)
-        df_coverage, S3Apoly, S3Bpoly = coverage(path_out,df_doy,df_coverage)  
+        df_coverage, S3Apoly, S3Bpoly = coverage(path_out,df_doy,df_coverage)
+        
+        
+        # add polygons to Dataframe
+        row_possbile = {
+            'doy': str(doy),
+            'A': S3Apoly,
+            'B': S3Bpoly,
+            'A+B': S3Apoly.union(S3Bpoly),
+            'A^B': S3Apoly.intersection(S3Bpoly),
+            '(A+B)-(A^B)': S3Apoly.union(S3Bpoly) - S3Apoly.intersection(S3Bpoly)
+            }
+        
+        df_possible = df_possible.append(row_possbile,ignore_index=True) 
 
 csv_coverage_filaname = os.path.join(path_out,'OLCI_coverage_info.csv')
 df_coverage.round({'A union B':1,'A interc B':1}).to_csv(csv_coverage_filaname)
+
+#%% calculated overlappin per n-day cicle
+ncycle = 27
+df_poly = df.loc[df['doy'] <= ncycle]
+polygons = create_all_polygon(df_poly) 
+plt.gcf()
+plt.title(f'S3A+S3B Coverage for a {str(ncycle)}-day cycle')
+
+M, N = 500, 500
+
+lat_min, lat_max = 30, 50
+lon_min, lon_max = -10, 50
+
+x = np.linspace(lon_min, lon_max,M+1)
+y = np.linspace(lat_min,lat_max,N+1)
+X,Y = np.meshgrid(x,y)
+
+positions = np.vstack([Y.ravel(), X.ravel()])
+
+count_vec = intersection_with_polygon(polygons,positions)
+
+plt.figure()
+m = create_map()
+plt.scatter(*positions[::-1],c=count_vec,marker='.')
+cbar = plt.colorbar()
+plt.title(f'S3A+S3B Coverage for a {str(ncycle)}-day cycle')
+cbar.set_label('Counts')
+plt.show()
 #%%
 # if __name__ == '__main__':
 #     main()
+
+        
